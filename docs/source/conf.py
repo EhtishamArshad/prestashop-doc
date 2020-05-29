2df966caa592609809e1368ea56724f64c8e17f8
Ehtisham Arshad <ehtishamarshad@Ehtishams-MacBook-Pro.local>
2020-05-29 11:49:11 +0500

    revamped conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 
 
 # -- Project information -----------------------------------------------------
+master_doc = 'index'
 
 project = 'Prestashop Documentation'
 copyright = '2020, Ehtisham Arshad'
@@ -50,5 +51,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-master_doc = 'index'
